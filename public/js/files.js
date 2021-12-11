2caa4e6cab3beb0c3c432cdb3d1335bd6bfa2afb
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-12-11 12:35:49 +0330

    asefd

--- public/js/files.js
+++ public/js/files.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
     $('#link3').addClass('active');
 
-    
+    $('#add-user-btn').click(() => {
+        $('.black-modal').fadeIn(500);
+        $('#add-file-popup').fadeIn(500);
+    })
+    $('.black-modal').click(() => {
+        $('.black-modal').fadeOut(300);
+        $('#add-file-popup').fadeOut(300);
+    })
+    $('.close-popup').click(() => {
+        $('.black-modal').fadeOut(300);
+        $('#add-file-popup').fadeOut(300);
+    })
 });
