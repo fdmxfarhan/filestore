673f467ea3227e68e581665088d810f47f055bd7
Farhan Daemi <fdmxfarhan@yahoo.com>
2021-12-31 12:35:21 +0330

    asdf

--- routes/home.js
+++ routes/home.js
@@ -3,8 +3,8 @@ var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/', (req, res, next) => {
-    res.redirect('/users/login');
-    // res.render('home');
+    // res.redirect('/users/login');
+    res.render('home');
 });
 
 module.exports = router;
