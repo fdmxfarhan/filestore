3f6b473460283e51b9830859e96b6d1947872d30
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-01-01 00:21:32 +0330

    asdf

--- models/File.js
+++ models/File.js
@@ -7,7 +7,7 @@ var FileSchema = new mongoose.Schema({
     phone: String,
     address: String,
     type: String,
-    date: Date,
+    date: String,
     dateJ: Object,
     state: String,
     fileNumber: Number,
@@ -26,9 +26,37 @@ var FileSchema = new mongoose.Schema({
     floortype: String, // Kaf
     service: String, 
     heatingAndCoolingSystem: String, // systeme garmayesh va sarmayesh
+    meterage2: String,
+    bedroom2: String,
+    floor2: String,
+    numOfFloors2: String, // tabaghat
+    unit2: String, // vahed
+    buildAge2: String,
+    parking2: String,
+    warehouse2: String, // anbari
+    elevator2: String,
+    kitchen2: String,
+    view2: String, // nama
+    floortype2: String, // Kaf
+    service2: String, 
+    heatingAndCoolingSystem2: String, // systeme garmayesh va sarmayesh
+    meterage3: String,
+    bedroom3: String,
+    floor3: String,
+    numOfFloors3: String, // tabaghat
+    unit3: String, // vahed
+    buildAge3: String,
+    parking3: String,
+    warehouse3: String, // anbari
+    elevator3: String,
+    kitchen3: String,
+    view3: String, // nama
+    floortype3: String, // Kaf
+    service3: String, 
+    heatingAndCoolingSystem3: String, // systeme garmayesh va sarmayesh
     options: String, 
-    price: String, 
-    fullPrice: String, 
+    price: Number, 
+    fullPrice: Number, 
     lone: String,
     changable: String,
     discount: String,
--- public/js/files.js
+++ public/js/files.js
@@ -8,9 +8,22 @@ $(document).ready(function(){
     $('.black-modal').click(() => {
         $('.black-modal').fadeOut(300);
         $('#add-file-popup').fadeOut(300);
+        files.forEach(file => file.view.fadeOut(300));
     })
     $('.close-popup').click(() => {
         $('.black-modal').fadeOut(300);
         $('#add-file-popup').fadeOut(300);
-    })
+        files.forEach(file => file.view.fadeOut(300));
+    });
+    var fileLength = parseInt(document.getElementById('file-length').textContent);
+    var files = [];
+    for(var i=0; i<fileLength; i++){
+        files.push({btn: $(`#edit-file-btn${i}`), view: $(`#edit-file-popup${i}`)});
+    }
+    files.forEach(file => {
+        file.btn.click(() => {
+            $('.black-modal').fadeIn(500);
+            file.view.fadeIn(500);
+        });
+    });
 });
