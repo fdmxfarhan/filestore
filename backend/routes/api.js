8cea2dd157a9c5fa3cbbb7d929df6cc6d80c01d7
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-01-07 12:19:49 +0330

    asdf

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
-const { ensureAuthenticated } = require('../config/auth');
 var User = require('../models/User');
 var Estate = require('../models/Estate');
 var File = require('../models/File');
 const ZarinpalCheckout = require('zarinpal-checkout');
 const zarinpal = ZarinpalCheckout.create('18286cd3-6065-4a7a-ad43-05eaf70f01a6', false);
-
+const { ensureAuthenticated } = require('../config/auth');
+const sms = require('../config/sms');
 
 router.get('/', (req, res, next) => {
     res.send('API called successfully');
@@ -69,6 +69,7 @@ router.get('/payment-call-back', (req, res, next) => {
     if(Status == 'OK'){
         Estate.findOne({authority: Authority}, (err, estate) => {
             Estate.updateMany({authority: Authority}, {$set: {payed: true, payDate: new Date(), authority: ''}}, (err, doc) => {
+                sms(estate.phone, `اشتراک ${estate.planType} شما فعال شد\n فایل استور`);
                 res.render('./api/success-payment', {estate});
             });
         });
