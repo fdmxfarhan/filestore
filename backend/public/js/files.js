521a997bc80f67b87a35eedd2cff8490495e1a7a
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-01-30 10:58:36 +0330

    asdf

--- backend/public/js/files.js
+++ backend/public/js/files.js
@@ -1,3 +1,29 @@
+function search() {
+    // alert('hello');
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchInput");
+    filter = input.value;
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    // console.log(tr);
+    for (i = 1; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0].getElementsByTagName("h1")[0];
+        td2 = tr[i].getElementsByTagName("td")[1];
+        td3 = tr[i].getElementsByTagName("td")[2];
+        // console.log(td);
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            txtValue2 = td2.textContent || td2.innerText;
+            txtValue3 = td3.textContent || td3.innerText;
+            if (txtValue.indexOf(filter) > -1 || txtValue2.indexOf(filter) > -1 || txtValue3.indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
+
 $(document).ready(function(){
     $('#link3').addClass('active');
 
