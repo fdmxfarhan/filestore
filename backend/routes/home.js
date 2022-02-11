0a6b5630b246f6ee2244f0edea2356bd72d05624
Farhan Daemi <fdmxfarhan@yahoo.com>
2022-02-11 17:15:14 +0330

    filestore company

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 const sms2 = require('../config/sms2');
+const sms = require('../config/sms');
+
 
 router.get('/', (req, res, next) => {
     // res.redirect('/users/login');
@@ -16,6 +18,7 @@ router.post('/newsletter', (req, res, next) => {
     sms2(phone, 'درخواست عضویت در فایل استور در حال بر رسی است.');
     sms2('09336448037', `درخواست عضویت در فایل استور:\n${phone}`);
     sms2('09129630587', `درخواست عضویت در فایل استور:\n${phone}`);
+    sms2('09351248932', `درخواست عضویت در فایل استور:\n${phone}`);
     res.redirect('/');
 });
 router.post('/register-estate', (req, res, next) => {
@@ -24,6 +27,7 @@ router.post('/register-estate', (req, res, next) => {
     text = `درخواست عضویت در فایل استور:\nتلفن: ${phone}\nنام: ${name}`;
     sms2('09336448037', text);
     sms2('09129630587', text);
+    sms2('09351248932', text);
     res.redirect('/');
 });
 router.post('/register-estate', (req, res, next) => {
@@ -32,6 +36,7 @@ router.post('/register-estate', (req, res, next) => {
     text = `درخواست سپردن ملک در فایل استور:\nتلفن: ${phone}\nنام: ${name}\nمنطقه: ${area}`;
     sms2('09336448037', text);
     sms2('09129630587', text);
+    sms2('09351248932', text);
     res.redirect('/');
 });
 
